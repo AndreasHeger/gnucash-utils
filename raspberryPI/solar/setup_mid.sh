f9d149c05b9ad36c82a7a928db2ba9b4c8bab693
Andreas Heger <andreas.heger@gmail.com>
2014-03-08 01:09:38 +0000

    added status

--- raspberryPI/solar/setup_mid.sh
+++ raspberryPI/solar/setup_mid.sh
@@ -16,6 +16,7 @@ cp monitor_temperature.py /usr/share/solar/monitor_temperature.py
 chmod u+x /etc/init.d/monitor_temperature
 update-rc.d monitor_temperature defaults 80
 
+cp images/*.png /mnt/ramdisk
 cp temperatureweb.py /usr/lib/cgi-bin/
 chown -R www-data:www-data /usr/lib/cgi-bin/temperatureweb.py /usr/lib/cgi-bin/solarweb.py /mnt/ramdisk
 
