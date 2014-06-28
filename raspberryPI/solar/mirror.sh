ba950fd6fcd15cad1a4d3d8241a99914d5b5cc44
Andreas Heger <andreas.heger@gmail.com>
2014-06-28 21:02:34 +0100

    more logmessages for mirroring

--- raspberryPI/solar/mirror.sh
+++ raspberryPI/solar/mirror.sh
@@ -1,15 +1,21 @@
 #! /bin/bash
 
 echo "mirroring from monitoring stations"
+echo "mid-pi"
 python solar_mirror.py --config=mirror_mid-pi.ini mirror
+echo "top-pi"
 python solar_mirror.py --config=mirror_top-pi.ini mirror
+echo "bottom-pi"
 python solar_mirror.py --config=mirror_bottom-pi.ini mirror
+echo "mirroring finished"
 
+echo "started saving data"
 echo "mounting diskstation"
 mount /mnt/diskstation
 if test -d /mnt/diskstation/solar; then
-	echo "mirroring to diskstation"
+	echo "mirroring log files to diskstation"
 	cp -a *.log /mnt/diskstation/solar/
+	echo "mirroring data to diskstation"
 	cp -a csvdb /mnt/diskstation/solar/ 
 fi
 
--- raspberryPI/solar/setup_mid.sh
+++ raspberryPI/solar/setup_mid.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-
 echo "pi-mid"
+
+# once only: add the following to fstab
+# tmpfs           /mnt/ramdisk tmpfs      defaults,size=256M 0 0
+# 192.168.0.2:/volume1/data       /mnt/diskstation        nfs     user,noauto
+
+mkdir /mnt/ramdisk
+mkdir /mnt/diskstation
+
 echo "setting up monitoring of solar and temperature"
 
 cp monitor_solar.sh /etc/init.d/monitor_solar
@@ -21,3 +28,24 @@ cp *web.py Utils.py /usr/lib/cgi-bin/
 chown -R www-data:www-data /usr/lib/cgi-bin/*.py /mnt/ramdisk
 
 # sudo rename 's/S01/S90/' /etc/rc*.d/S*monito*
+
+# setup mirroring
+# Note: need to setup passwordless ssh beforehand
+# ssh-copy-id -i .ssh/pi_rsa pi@192.168.0.55
+# ssh-copy-id -i .ssh/pi_rsa pi@192.168.0.57
+# ssh-copy-id -i .ssh/pi_rsa pi@192.168.0.59
+# scp -i .ssh/pi_rsa .ssh/pi_rsa pi@192.168.0.55:.ssh/
+# scp -i .ssh/pi_rsa .ssh/pi_rsa pi@192.168.0.57:.ssh/
+# scp -i .ssh/pi_rsa .ssh/pi_rsa pi@192.168.0.59:.ssh/
+
+# sudo rename 's/S01/S90/' /etc/rc*.d/S*monito*
+# sudo apt-get install sqlite3 python-lxml
+# make sure rpcbind is started at boot
+# sudo update-rc.d rpcbind enable
+
+cd
+mkdir mirror
+cd mirror
+ln -fs ../Projects/raspberryPI/solar/mirror_*.ini .
+ln -fs ../Projects/raspberryPI/solar/mirror.sh .
+ln -fs ../Projects/raspberryPI/solar/solar_mirror.py .
