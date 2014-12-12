fd0830a4c5aca260b3f5aa6d411cb77cf7c27f6e
Andreas Heger <andreas.heger@gmail.com>
2014-12-12 20:21:09 +0000

    added comments

--- raspberryPI/solar/rrdwattsond-0.8/make_rrd.sh
+++ raspberryPI/solar/rrdwattsond-0.8/make_rrd.sh
@@ -10,6 +10,20 @@ fi
 # 1.1.2011 00:00:00 EET
 MINTIME=1293832800
 
+# Use default base interval (-s 300) = 300s = 5mins
+# heartbeat is 600 s (10 mins)
+# Compute min/avg/max for
+# RRA: 50% of undefined values(0.5)
+# A: interval=5m, keep 168h = 7 days  
+# B: interval=30m, keep for 14 days
+# C: interval=1h, keep for 30 days (~1 month)
+# D: interval=2h, keep for 66 days (~2 months)
+# E: interval=24h, keep for 3650 days (~10 years)
+# LAST: keep value every 5 mins for 50 hours
+
+# watthour is computed from avgwatt (collected every five minutes)
+# by dividing by 12
+
 rrdtool create wattson.rrd -b $MINTIME \
 DS:minwatt:GAUGE:600:U:U \
 DS:avgwatt:GAUGE:600:U:U \
