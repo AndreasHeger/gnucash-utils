87486c9a22f83074b89231569327061ec4e56b4d
Andreas Heger <andreas.heger@gmail.com>
2014-12-15 21:23:45 +0000

    added graphite monitoring

---
+++ raspberryPI/solar/Monitor.py
@@ -0,0 +1,64 @@
+
+import socket
+import time
+
+
+class Monitor:
+
+    CARBON_SERVER = '192.168.0.51'
+    CARBON_PORT = 2003
+
+    def __init__(self, logger, heart_beat=10):
+        self.stdin_path = '/dev/null'
+        # tty not available for a spice
+        self.stdout_path = '/dev/null'
+        self.stderr_path = '/dev/null'
+        self.pidfile_path = '/mnt/ramdisk/temperature_daemon.pid'
+        self.pidfile_timeout = 5
+        self.logger = logger
+        self.heart_beat = heart_beat
+
+        # open tcp connection
+        self.connection = None
+
+    def run(self):
+        self.setup()
+
+        self.logger.info(
+            "starting main loop with heartbeat=%i" % self.heart_beat)
+
+        while True:
+            values = self.monitor()
+
+            self.logger.debug("acquired values: %s" % str(values))
+            self.updateDatabase(values)
+
+            time.sleep(self.heart_beat)
+
+    def setup(self):
+        self.logger.info("monitoring initialization")
+
+    def updateDatabase(self, values):
+        '''send data to carbon server'''
+        sock = socket.socket()
+        try:
+            sock.connect((self.CARBON_SERVER,
+                          self.CARBON_PORT))
+        except:
+            raise OSError(
+                "Couldn't connect to %(server)s on port %(port)d, "
+                "is carbon running?" %
+                {'server': self.CARBON_SERVER,
+                 'port': self.CARBON_PORT})
+
+        now = int(time.time())
+        lines = ["%s %s %d" % (sensor, value, now) for 
+                 sensor, value in values.items()]
+
+        # all lines must end in a newline
+        message = '\n'.join(lines) + '\n'
+        # print "sending message\n"
+        # print '-' * 80
+        # print message
+        # print
+        sock.sendall(message)
---
+++ raspberryPI/solar/monitor_temperature.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+'''Monitor temperature.
+
+Adding a new sensor
+===================
+
+1. Mirror all data. The steps below will delete the values in the
+   existing rrd database.
+
+2. Add columns for the new sensor to the relevant tables in the sqlite
+   database. For example::
+
+      for x in `sqlite3 csvdb ".tables" | xargs --max-args=1 | grep top_temperature`; do  \
+           sqlite3 csvdb "ALTER TABLE $x ADD COLUMN BoilerWater FLOAT;"; done
+
+3. Stop the monitoring daemon
+
+3. Delete old rrd database. For example::
+
+      rm /mnt/ramdisk/temperature.rrd
+
+4. Restart the monitoring daemon. The new rrd will be
+   automatically created.
+
+'''
+
+import logging
+import subprocess
+import socket
+from Monitor import Monitor
+
+# third party libs
+from daemon import runner
+
+HOSTNAME = socket.gethostname()
+
+CONFIG = {
+    "TemperatureKeevaRoomBack": '10-0008029db5bc',
+    "TemperatureKeevaRoomTop": 'o28.3DA0D4040000',
+    "TemperatureKeevaCupBoard": '10-0008029d938d',
+    "TemperatureAiringCabinet": '10-0008029db6c3',
+    "TemperatureAttic": '10-0008029da542',
+    "TemperatureNiamhRoomBack": "o28.6BFED3040000",
+    "TemperatureNiamhRoomTop": "o28.45C0D4040000",
+    "TemperatureLivingRoomWindow": "28-000004d41fb1",
+    "TemperatureLivingRoomCupBoard": "28-000004d45cae",
+    "TemperatureLandingTop": "o10.ED859D020800",
+    "TemperatureLandingMiddle": "28-000004d3f73e",
+    "TemperatureLandingBottom": "10-0008029dc359",
+    "TemperatureHotWater": "o10.B1B29D020800",
+    "TemperatureBoilerWater": "o10.0ec89d020800",
+    "TemperatureHallwayTop": "28-000004d49218",
+    "TemperatureBackRoomFront": "28-000004d422f5",
+    "TemperatureHallwayBottom": "28-00000584f21d",
+    "TemperatureBackRoomBack": "28-0000058528bc",
+
+}
+
+SENSOR_GROUPS = [
+    ("TemperatureKeevaRoomBack",
+     "TemperatureKeevaRoomTop",
+     "TemperatureKeevaCupBoard",
+     "TemperatureAiringCabinet",
+     "TemperatureAttic",
+     "TemperatureNiamhRoomBack",
+     "TemperatureNiamhRoomTop",
+     "TemperatureLandingTop",
+     "TemperatureHotWater",
+     "TemperatureBoilerWater"),
+    ("TemperatureLivingRoomWindow",
+     "TemperatureLivingRoomCupBoard",
+     "TemperatureLandingMiddle",
+     "TemperatureLandingBottom"),
+    ("TemperatureHallwayTop",
+     "TemperatureHallwayBottom",
+     "TemperatureBackRoomBack",
+     "TemperatureBackRoomFront"),
+]
+
+CACHE = {}
+
+# set heart-beat
+HEART_BEAT = 10
+
+
+class App(Monitor):
+
+    def __init__(self, *args, **kwargs):
+        Monitor.__init__(self, *args, **kwargs)
+
+        if HOSTNAME == "top-pi":
+            self.sensor_group = 0
+        elif HOSTNAME == "mid-pi":
+            self.sensor_group = 1
+        elif HOSTNAME == "bottom-pi":
+            self.sensor_group = 2
+        else:
+            raise ValueError('unknown host')
+            
+    def monitor(self):
+        '''acquire values
+        '''
+
+        values = {}
+        success = {}
+        for key in SENSOR_GROUPS[self.sensor_group]:
+            logger.debug("acquiring %s" % key)
+            fn = CONFIG[key]
+
+            if fn[0] == "o":
+                try:
+                    temperature = subprocess.check_output(
+                        ["owread", "/%s/temperature" % fn[1:]])
+                    success[key] = 'ok'
+                except subprocess.CalledProcessError:
+                    logger.debug("%s from cache" % key)
+                    temperature = CACHE.get(key, 0)
+                    success[key] = 'fail'
+                temperature = float(temperature)
+            else:
+                try:
+                    tfile = open(
+                        "/sys/bus/w1/devices/w1_bus_master1/%s/w1_slave" % fn)
+                except IOError:
+                    tfile = None
+                if tfile:
+                    text = tfile.readlines()
+                    tfile.close()
+                    status, temperature_data = text
+                    if not status.endswith("YES\n"):
+                        logger.debug("%s from cache" % key)
+                        temperature = CACHE.get(key, 0)
+                    else:
+                        temperature_data = temperature_data.split()[-1]
+                        temperature = float(temperature_data[2:])
+                        temperature = temperature / 1000
+                    success[key] = 'ok'
+                else:
+                    logger.debug("%s from cache" % key)
+                    success[key] = 'fail'
+                    temperature = CACHE.get(key, 0)
+
+            CACHE[key] = temperature
+            values[key] = temperature
+            self.logger.debug(
+                "status: %s" %
+                (",".join(
+                    ['%s=%s' % (x, y) for x, y in success.items()])))
+
+        return values
+
+logger = logging.getLogger("DaemonLog")
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler = logging.FileHandler("/mnt/ramdisk/temperature.log")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+app = App(logger=logger, heart_beat=HEART_BEAT)
+app.run()
+
+daemon_runner = runner.DaemonRunner(app)
+# This ensures that the logger file handle does not get closed during
+# daemonization
+daemon_runner.daemon_context.files_preserve = [handler.stream]
+daemon_runner.do_action()
