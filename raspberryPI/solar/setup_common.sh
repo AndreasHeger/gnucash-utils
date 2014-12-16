a25635ce1e1fc3f4431acb172333260cce374c7e
Andreas Heger <andreas.heger@gmail.com>
2014-12-16 21:36:21 +0000

    added graphite configuration files

--- raspberryPI/solar/setup_common.sh
+++ raspberryPI/solar/setup_common.sh
@@ -18,30 +18,30 @@
 
 # turn off 
 if [ ! -e /etc/default/rcS.orig ]; then
-cp /etc/default/rcS /etc/default/rcS.orig
-sh -c "echo 'RAMTMP=yes' >> /etc/default/rcS"
+    cp /etc/default/rcS /etc/default/rcS.orig
+    sh -c "echo 'RAMTMP=yes' >> /etc/default/rcS"
 fi
 
 mkdir /mnt/ramdisk
 
 # create fstab
 if [ ! -e /etc/fstab.orig ] ; then
-mv /etc/fstab /etc/fstab.orig
-sh -c "echo 'tmpfs           /tmp            tmpfs   nodev,nosuid,size=30M,mode=1777       0    0' >> /etc/fstab"
-sh -c "echo 'tmpfs           /var/log        tmpfs   nodev,nosuid,size=30M,mode=1777       0    0' >> /etc/fstab"
-sh -c "echo 'proc            /proc           proc    defaults                              0    0' >> /etc/fstab"
-sh -c "echo '/dev/mmcblk0p1  /boot           vfat    defaults                              0    2' >> /etc/fstab"
-sh -c "echo '/dev/mmcblk0p2  /               ext4    defaults,ro,noatime,errors=remount-ro 0    1' >> /etc/fstab"
-# add ramdisk for monitoring
-sh -c "echo 'tmpfs           /mnt/ramdisk    tmpfs   defaults,size=200M                    0    0' >> /etc/fstab"
-# add mount point for diskstation
-sh -c "echo '192.168.0.2:/volume1/data       /mnt/diskstation        nfs     user,noauto' >> /etc/fstab"
-sh -c "echo ' ' >> /etc/fstab"
+    mv /etc/fstab /etc/fstab.orig
+    sh -c "echo 'tmpfs           /tmp            tmpfs   nodev,nosuid,size=30M,mode=1777       0    0' >> /etc/fstab"
+    sh -c "echo 'tmpfs           /var/log        tmpfs   nodev,nosuid,size=30M,mode=1777       0    0' >> /etc/fstab"
+    sh -c "echo 'proc            /proc           proc    defaults                              0    0' >> /etc/fstab"
+    sh -c "echo '/dev/mmcblk0p1  /boot           vfat    defaults                              0    2' >> /etc/fstab"
+    sh -c "echo '/dev/mmcblk0p2  /               ext4    defaults,ro,noatime,errors=remount-ro 0    1' >> /etc/fstab"
+    # add ramdisk for monitoring
+    sh -c "echo 'tmpfs           /mnt/ramdisk    tmpfs   defaults,size=200M                    0    0' >> /etc/fstab"
+    # add mount point for diskstation
+    sh -c "echo '192.168.0.2:/volume1/data       /mnt/diskstation        nfs     user,noauto' >> /etc/fstab"
+    sh -c "echo ' ' >> /etc/fstab"
 fi
 
 if [ ! -e /etc/mtab.orig ]; then
 mv /etc/mtab /etc/mtab.orig
-ln -s /proc/self/mounts /etc/mtab
+ln -fs /proc/self/mounts /etc/mtab
 fi
 
 cat <<EOT1 > /usr/bin/rpi-rw
@@ -96,6 +96,8 @@ chmod u+x /etc/init.d/monitor_temperature
 
 #######################################################
 echo "setting up monitoring of wattson"
+cp monitor_wattson.sh /etc/init.d/monitor_wattson
+chmod 755 /etc/init.d/monitor_wattson
 cp monitor_wattson.py /usr/share/solar/monitor_wattson.py
 chmod u+x /etc/init.d/monitor_wattson
 
@@ -105,7 +107,7 @@ cp *web.py Utils.py /usr/lib/cgi-bin/
 cp Monitor.py Utils.py /usr/share/solar/
 chown -R www-data:www-data /usr/lib/cgi-bin/*.py /mnt/ramdisk
 cp images/*.png /mnt/ramdisk
-ln -s /mnt/ramdisk /var/www/images
+ln -fs /mnt/ramdisk /var/www/images
 
 # The following needs to be done to activate the various services
 # depending on which machine we are on:
