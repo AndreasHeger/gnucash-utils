43f563cb4bc4df3f93390e9526616b1f11541808
Andreas Heger <andreas.heger@gmail.com>
2014-12-28 19:26:56 +0000

    added carbon-cache init script

--- raspberryPI/solar/setup_front.sh
+++ raspberryPI/solar/setup_front.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo "setting up: mid-pi"
+echo "setting up: front-pi"
 echo "front-pi" > /etc/hostname
-
-echo "installing watson"
-
-# (cd ./rrdwattsond-0.8; install.sh) 
 
 echo "activating wattson monitoring"
 # update-rc.d monitor_weather defaults 80
@@ -13,3 +9,6 @@ echo "activating wattson monitoring"
 # update-rc.d monitor_solar defaults 80
 update-rc.d monitor_wattson defaults 80
 sudo rename 's/S01/S90/' /etc/rc*.d/S*monito*
+
+echo "activating carbon-cache daemon"
+update-rc.d carbon-cache defaults 80
