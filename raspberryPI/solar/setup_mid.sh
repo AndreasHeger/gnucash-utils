7e4306ff80b9b0691e064b550c74226e553b8b33
Andreas Heger <andreas.heger@gmail.com>
2015-01-05 21:21:47 +0000

    use .55 as graphite server

--- raspberryPI/solar/setup_mid.sh
+++ raspberryPI/solar/setup_mid.sh
@@ -3,15 +3,20 @@
 echo "setting up: mid-pi"
 echo "mid-pi" > /etc/hostname
 
+# make sure rpcbind is started at boot
+update-rc.d rpcbind enable
+
 echo "activating solar and temperature monitoring"
 # update-rc.d monitor_weather defaults 80
 update-rc.d monitor_temperature defaults 80
 update-rc.d monitor_solar defaults 80
 sudo rename 's/S01/S90/' /etc/rc*.d/S*monito*
 
+echo "activating carbon-cache daemon"
+update-rc.d carbon-cache defaults 80
+
 echo "activating dashing"
 update-rc.d dashing defaults 80
-
 
 echo "setting up mirroring"
 # setup mirroring
@@ -24,25 +29,22 @@ echo "setting up mirroring"
 # scp -i .ssh/pi_rsa .ssh/pi_rsa pi@192.168.0.57:.ssh/
 # scp -i .ssh/pi_rsa .ssh/pi_rsa pi@192.168.0.59:.ssh/
 
-apt-get install sqlite3 python-lxml
-# make sure rpcbind is started at boot
-update-rc.d rpcbind enable
 
-echo "setting up mirroring"
 
-cd
-mkdir mirror
-cd mirror
-ln -fs ../Projects/raspberryPI/solar/mirror_*.ini .
-ln -fs ../Projects/raspberryPI/solar/mirror.sh .
-ln -fs ../Projects/raspberryPI/solar/solar_mirror.py .
+# echo "setting up mirroring"
+# apt-get install sqlite3 python-lxml
+# cd
+# mkdir mirror
+# cd mirror
+# ln -fs ../Projects/raspberryPI/solar/mirror_*.ini .
+# ln -fs ../Projects/raspberryPI/solar/mirror.sh .
+# ln -fs ../Projects/raspberryPI/solar/solar_mirror.py .
 
-cat <<EOC | crontab
-SHELL=/bin/bash
-# daily backup from ramdisk to SD card
-@daily  /etc/init.d/ramdisk sync >> /dev/null 2>&1
-# mirroring: run 30 minutes after one am, every day
-30 1 * * *    (cd /home/pi/mirror; ./mirror.sh >> /mnt/ramdisk/mirroring.log 2>&1)
-EOC
-
+# cat <<EOC | crontab
+# SHELL=/bin/bash
+# # daily backup from ramdisk to SD card
+# @daily  /etc/init.d/ramdisk sync >> /dev/null 2>&1
+# # mirroring: run 30 minutes after one am, every day
+# 30 1 * * *    (cd /home/pi/mirror; ./mirror.sh >> /mnt/ramdisk/mirroring.log 2>&1)
+# EOC
 
