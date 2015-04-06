6ad2ee829dfb2e0fbca13ea8ef2e535447dbd268
Andreas Heger <andreas.heger@gmail.com>
2015-04-06 22:00:56 +0100

    fine tuning solar charts

--- raspberryPI/solar/dashboard/jobs/graphite_energy.rb
+++ raspberryPI/solar/dashboard/jobs/graphite_energy.rb
@@ -14,62 +14,19 @@ INTERVAL = '30s'
 job_mapping = [
    {"name" => "energy",
     "interval" => "5s",
-    "since" => "-10hour",
+    "since" => "-4hour",
     "metrics" => ['Wattson.Power.Grid',
                   'Wattson.Power.Solar']
                },
    {"name" => "solarpower",
     "interval" => "5s",
-    "since" => "-10hour",
+    "since" => "-24hour",
     "metrics" => ['Solar.Power.East',
                   'Solar.Power.West',
                   'Solar.Power.Total']
                }
 ]
 
-
-# Extend the float to allow better rounding. Too many digits makes a messy dashboard
-class Float
-    def sigfig_to_s(digits)
-        f = sprintf("%.#{digits - 1}e", self).to_f
-        i = f.to_i
-        (i == f ? i : f)
-    end
-end
-
-# class Graphite
-#     # Initialize the class
-#     def initialize(host, port)
-#         @host = host
-#         @port = port
-#     end
-
-#     # Use Graphite api to query for the stats, parse the returned JSON
-#     # and return the result
-#     def query(statname, since=nil)
-#         since ||= '-1hour'
-#         http = Net::HTTP.new(@host, @port)
-#         response = http.request(Net::HTTP::Get.new("/render?format=json&target=#{statname}&from=#{since}"))
-#         result = JSON.parse(response.body, :symbolize_names => true)
-#         return result.first
-#     end
-
-#     # Gather the datapoints and turn into Dashing graph widget format
-#     def points(name, since=nil)
-#         since ||= '-1min'
-#         stats = query(name, since)
-#         datapoints = stats[:datapoints]
-#         points = []
-#         count = 1
-#         (datapoints.select { |el| not el[0].nil? }).each do|item|
-#             points << {x: item[1], y: item[0] || 0}
-#             count += 1
-#         end
-
-#         return points
-#     end
-
-# end
 
 job_mapping.each do |entry|
 
